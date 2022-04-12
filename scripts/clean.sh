c3421f8d74388321d91cd70e6db0f93b05cd4957
thom <thomblweed@gmail.com>
2022-04-12 20:47:09 +0100

    scripts updates

---
+++ scripts/clean.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+if [ -d terraform/.terraform ]; then
+    rm -rf terraform/.terraform
+fi
+
+find terraform -name '*.tfstate*' -type f -delete
+find terraform -name '.terraform*' -type f -delete
