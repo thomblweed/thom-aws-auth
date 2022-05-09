fbaeeb83a697b0e56a18ac0798b02d7fa0392fec
thom <thomblweed@gmail.com>
2022-05-09 21:22:14 +0100

    npm scripts updates for db migrations

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
-if [ -d terraform-aws/.terraform ]; then
-    rm -rf terraform-aws/.terraform
+if [ -d terraform/aws/.terraform ]; then
+    rm -rf terraform/aws/.terraform
 fi
 
-if [ -d terraform-docker/.terraform ]; then
-    rm -rf terraform-docker/.terraform
+if [ -d terraform/docker/.terraform ]; then
+    rm -rf terraform/docker/.terraform
 fi
 
-if [ -d terraform-docker/data ]; then
-    rm -rf terraform-docker/data
+if [ -d terraform/docker/data ]; then
+    sudo rm -rf terraform/docker/data
 fi
 
 find terraform* -name '*.tfstate*' -type f -delete
