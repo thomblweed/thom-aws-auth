40066705c8b0b4e9720810715928216ce0baef50
thom <thomblweed@gmail.com>
2022-05-12 22:14:47 +0100

    migrations updates to be updated later

--- scripts/migrateLocalDB.sh
+++ scripts/migrateLocalDB.sh
@@ -5,3 +5,8 @@ aws dynamodb create-table --endpoint-url http://localhost:8000 \
     --attribute-definitions AttributeName=UserId,AttributeType=S AttributeName=Email,AttributeType=S \
     --key-schema AttributeName=UserId,KeyType=HASH AttributeName=Email,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+aws dynamodb put-item --endpoint-url http://localhost:8000 \
+    --table-name 'UsersAuth' \
+    --item \
+    '{"UserId": {"S": "No One You Know"}, "Email": {"S": "thomblweed@gmail.com"}}'
